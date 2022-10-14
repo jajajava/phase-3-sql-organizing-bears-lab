ef009c8f55fbde88a1728d03778454a90a276bbc
David <davidstepanov@yahoo.com>
2022-10-14 10:59:07 -0400

    commit tree c32f8c4d251ba28443c48cfa783c7bb4e710f1f4

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+    CREATE TABLE "bears" (
+	"id"	INTEGER,
+	"name"	TEXT,
+	"age"	INTEGER,
+	"sex"	VARCHAR(1),
+	"color"	TEXT,
+	"temperament"	TEXT,
+	"alive"	INTEGER,
+	PRIMARY KEY("id")
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO "bears" VALUES (1,'Mr.Chocolate',2,'M','brown','sweet',1);
+INSERT INTO "bears" VALUES (2,'Rowdy',3,'F','white','rowdy',1);
+INSERT INTO "bears" VALUES (3,'Tabitha',4,'F','black','boring',1);
+INSERT INTO "bears" VALUES (4,'Sergeant Brown',5,'M','brown','bossy',0);
+INSERT INTO "bears" VALUES (5,'Melissa',6,'F','brown','nice',1);
+INSERT INTO "bears" VALUES (6,'Grinch',7,'M','brown','mean',1);
+INSERT INTO "bears" VALUES (7,'Wendy',8,'F','brown','passive',1);
+INSERT INTO "bears" VALUES (8,NULL,NULL,NULL,NULL,NULL,NULL);
